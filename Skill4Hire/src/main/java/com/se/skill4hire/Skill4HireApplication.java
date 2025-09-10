d17d354cac1dccab1dd2d238782b81777ed3c53c
s-sandali <sandalisandagomi@gmail.com>
2025-09-10 14:51:11 +0530

    SCRUM-85 Initial Spring Boot project setup

---
+++ Skill4Hire/src/main/java/com/se/skill4hire/Skill4HireApplication.java
@@ -0,0 +1,13 @@
+package com.se.skill4hire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skill4HireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Skill4HireApplication.class, args);
+    }
+
+}
---
+++ Skill4Hire/src/test/java/com/se/skill4hire/Skill4HireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.skill4hire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skill4HireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
