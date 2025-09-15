92097be015fd156ac549e64706ae286cbdacefe9
Shadhu23 <it23749498@my.sliit.lk>
2025-09-15 13:46:52 +0530

    Backend API for registration/login (/api/admins/register, /api/admins/login)

--- Skill4Hire/src/main/java/com/se/skill4hire/Skill4HireApplication.java
+++ Skill4Hire/src/main/java/com/se/skill4hire/Skill4HireApplication.java
@@ -2,8 +2,12 @@ package com.se.skill4hire;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.se.skill4hire.repository")
+@EntityScan("com.se.skill4hire.entity")
 public class Skill4HireApplication {
     public static void main(String[] args) {
         SpringApplication.run(Skill4HireApplication.class, args);
--- Skill4Hire/src/main/java/com/se/skill4hire/dto/profile/AdminProfileDTO.java
+++ Skill4Hire/src/main/java/com/se/skill4hire/dto/profile/AdminProfileDTO.java
@@ -1,4 +1,26 @@
 package com.se.skill4hire.dto.profile;
 
 public class AdminProfileDTO {
+
+    private String username;
+    private String email;
+    private String password;
+
+    public AdminProfileDTO() {}
+
+    public AdminProfileDTO(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters & Setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
