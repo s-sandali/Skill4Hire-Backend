717be19543b0c9f4653df4745d20a63539821c6e
s-sandali <sandalisandagomi@gmail.com>
2025-09-15 21:32:03 +0530

    SCRUM-72-Backend-API-for-registration-login-api-admins-register-api-admins-logint

--- Skill4Hire/src/main/java/com/se/skill4hire/repository/AdminRepository.java
+++ Skill4Hire/src/main/java/com/se/skill4hire/repository/AdminRepository.java
@@ -4,9 +4,7 @@ import com.se.skill4hire.entity.AdminProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-@Repository  // Optional, but good to have
+@Repository
 public interface AdminRepository extends JpaRepository<AdminProfile, Long> {
-    Optional<AdminProfile> findByEmail(String email);
+    AdminProfile findByEmail(String email);
 }
