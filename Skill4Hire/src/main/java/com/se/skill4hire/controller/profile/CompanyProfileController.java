b97b614d503480bdf89534c7abb5fad6584eb8c5
it23736832@my.sliit.lk <himasha221d@gmail.com>
2025-09-22 02:56:33 +0530

    SCRUM-49 Backend: Company Profile-in progress

---
+++ Skill4Hire/src/main/java/com/se/skill4hire/controller/profile/CompanyProfileController.java
@@ -0,0 +1,5 @@
+package com.se.skill4hire.controller.profile;
+
+public class CompanyProfileController {
+    // profile request and profile response files needed to add
+}
---
+++ Skill4Hire/src/main/java/com/se/skill4hire/entity/profile/CompanyProfile.java
@@ -0,0 +1,4 @@
+package com.se.skill4hire.entity.profile;
+
+public class CompanyProfile {
+}
