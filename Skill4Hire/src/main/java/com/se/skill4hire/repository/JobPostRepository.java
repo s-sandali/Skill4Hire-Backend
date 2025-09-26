e4f5e9938316b16fe7e83069e2a981d49998b76f
s-sandali <sandalisandagomi@gmail.com>
2025-09-26 15:45:20 +0530

    SCRUM-90 Backend: enhances the job repository with company connections

--- Skill4Hire/src/main/java/com/se/skill4hire/repository/JobPostRepository.java
+++ Skill4Hire/src/main/java/com/se/skill4hire/repository/JobPostRepository.java
@@ -1,13 +1,18 @@
 package com.se.skill4hire.repository;
 
 import com.se.skill4hire.entity.JobPost;
+import com.se.skill4hire.entity.auth.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface JobPostRepository extends JpaRepository<JobPost, Long> {
-    
-    List<JobPost> findByType(String type);
-    List<JobPost> findByLocation(String location);
+public interface JobPostRepository extends JpaRepository<JobPost, Long>, JpaSpecificationExecutor<JobPost> {
+    List<JobPost> findByCompany(Company company);
+    List<JobPost> findByCompanyAndStatus(Company company, JobPost.JobStatus status);
+    List<JobPost> findByStatus(JobPost.JobStatus status);
+    Optional<JobPost> findByIdAndCompany(Long id, Company company);
 }
